94282f9c1c02f98075198cb197540cb3ab6cdd6a
csoke.balazs <csoke.balazs@117-13>
2023-01-12 14:04:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Hehe
+{
+    class Program
+    {
+        static List<int> szam_list = new List<int>();
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            //Listák
+            for(int i = 0; i < 200; i++)
+            {
+                szam_list.Add(rnd.Next(1, 51));
+            }
+        }
+    }
+}
