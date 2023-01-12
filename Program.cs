819bba488a08dfc2f916bafc79dc8fb0dcd41996
Balázs Zsolt Csőke <csoke.balazs@verebelyszki.hu>
2023-01-12 14:53:02 +0100

    sda

--- Program.cs
+++ Program.cs
@@ -34,7 +34,7 @@
             }
             sw.Close();
             Console.WriteLine();
-            Console.WriteLine("\n----------------------------");
+            Console.WriteLine("\n-----------------------------------");
             Console.WriteLine("\t[Lista tisztítása]");
 
             foreach (var sz in szam_list)
@@ -44,9 +44,11 @@
             }
             Console.WriteLine($"\nTiszta lista nagysága: {tiszta_list.Count}.");
 
-            Console.WriteLine("\n----------------------------");
+            Console.WriteLine("\n-----------------------------------");
             Console.WriteLine("\t[Statisztika]");
             Console.WriteLine();
+            tiszta_list.Sort();
+            tiszta_list.Reverse();
             foreach (var t in tiszta_list)
             {
                 int dbstat = 0;
@@ -60,14 +62,20 @@
                 }
                 Console.WriteLine($"{t:00} : {dbstat} db");
             }
-
-
-
-
-
-
-
-
+            Console.WriteLine("\n-----------------------------------");
+            Console.WriteLine("\t[Elem hozzáadása + 245]");
+            szam_list.Add(245);
+            Console.WriteLine($"\núj lista hossza: {szam_list.Count}");
+            Console.WriteLine("\n-----------------------------------");
+            int osszeg = szam_list.Sum();
+            Console.WriteLine($"\nA lista összege: {osszeg}");
+            Console.WriteLine("\n-----------------------------------");
+            Console.WriteLine("\t[Maximum vagy minimum]");
+            int Max = szam_list.Max();
+            Console.WriteLine($"\nA legnagyobb értéke a listának: {Max}");
+            int MaxHely = szam_list.IndexOf(Max);
+            Console.WriteLine($"\nA legkissebb értéke a listának: {MaxHely+1}");
+            Console.WriteLine("\n-----------------------------------");
         }
     }
 }
